520d236a26f6e0e24736deebaab43ce4adcb736e
robson-muniz <robson_muniz@yahoo.com>
2019-09-30 18:09:06 -0300

    Delete items from our database.

--- app.js
+++ app.js
@@ -54,15 +54,26 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+  const item = new Item ({
+    name: itemName
+  });
+
+  item.save();
+
+  res.redirect("/");
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function (err) {
+    if (!err) {
+      console.log("Successfully deleted checked item.");
+      res.redirect("/");
   }
+});
 });
 
 app.get("/work", function(req,res){
