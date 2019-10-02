ed44d98d1c3aa390dc4d1101e60fec88cfdbcf88
robson-muniz <robson_muniz@yahoo.com>
2019-10-02 10:09:10 -0300

    Create Custom lists using Express Route Parameters.

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
 
 app.get("/", function(req, res) {
 
@@ -47,9 +54,32 @@ app.get("/", function(req, res) {
   });
   res.redirect("/");
   } else {
-    res.render("list", { listTitle: "Today", newListItems: foundItems });
+    res.render("list", {listTitle: "Today", newListItems: foundItems});
   }     
   });
+});
+
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName;
+
+  List.findOne({name:customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
+        // Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        // Show an existing list.
+        res.render("list", { listTitle: foundList.name, newListItems: foundList.items})
+      }
+    }
+  });
+  
+
 });
 
 app.post("/", function(req, res){
@@ -74,10 +104,6 @@ app.post("/delete", function(req, res){
       res.redirect("/");
   }
 });
-});
-
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 app.get("/about", function(req, res){
