d6bc54412d4964fa6935a3601778d1c92ac579f3
robson-muniz <robson_muniz@yahoo.com>
2019-10-04 12:04:37 -0300

    Setup MongoDB Atlas.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://admin-robson:*1eureka2*@cluster0-8ss5x.mongodb.net/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const itemsSchema = {
   name: String
